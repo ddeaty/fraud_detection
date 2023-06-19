ddd26529b6966035ead8a1f17effb2ea3baed6f9
ddeaty <ddeaty@gmail.com>
2023-06-20 00:36:34 +0530

    added 3 new ml code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 print("Adding ml releted code here")
 
+def model1():
+    print("Add code to model1")
+
+def model2():
+    print("Add code to model2")
+
+def model3():
+    print("Add code to model3")
+
+
