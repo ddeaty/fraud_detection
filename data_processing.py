d29a3a7cba29f21962053041c52d385e278159c0
ddeaty <ddeaty@gmail.com>
2023-06-19 22:41:07 +0530

    add a new function sum add and multiplication

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,15 @@
 
 print("Hello I am adding a new file name fraud detection")
 
+a = 5
+b =7
+def sum(a,b):
+    c = a+b
+    return c
+print("Sum function")
 
+def multiplication(a,b) 
+    d = a*b
+    return d
 
-
-
+print("Multiplication function")
